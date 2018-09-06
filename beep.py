c61497c22da9ac6bc9e4101df9a615b2fe7d0261
sanjuksha <sanjuksha@gmail.com>
2018-09-06 11:07:49 -0400

    waiting and parking

--- beep.py
+++ beep.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-import functools
+from __future__ import print_function
 import rospy 
+import sys
 from pyModbusTCP.client import ModbusClient
 from geometry_msgs.msg import Twist
 from waypoint_ui.srv import ModbusSetDigitalOutput
+import argparse
 
-
-class HMI(object):
-    def __init__(self):
+class beep(object):
+    def __init__(self,delay):
         self.pub = rospy.Publisher('/vector/navigation/cmd_vel', Twist, queue_size=10)
         self.vel_value=None
         self.for_waiting=0
@@ -20,7 +21,13 @@ class HMI(object):
         self.stop_value.angular.z=0
         self.time_when_stopped=Time=None
         self.time_when_started_moving=None
+        self.delay=delay
         rospy.Subscriber('/vector/navigation/cmd_vel_no_warning', Twist, self.callback, queue_size=10)
+
+    def set_digital_output(self,io_name, value):
+        rospy.wait_for_service('/waypoint_db/retrieve_waypoint', timeout=5.0)
+        self.set_output = rospy.ServiceProxy('modbus_manager/set_digital_output', ModbusSetDigitalOutput)
+        return  self.set_output(io_name,value)
 
     def check_if_moving(self):
         if self.vel_value.linear.x!=0 or self.vel_value.linear.y!=0 or self.vel_value.linear.z!=0 or self.vel_value.angular.x!=0 or self.vel_value.angular.y!=0 or self.vel_value.angular.z!=0:
@@ -35,7 +42,9 @@ class HMI(object):
         self.vel_value = data
         self.check_if_moving()
         if self.for_waiting==0:
-            rospy.sleep(5)
+            # self.set_digital_output("y1",True)
+            rospy.sleep(int(self.delay))
+            
         self.pub.publish(self.vel_value)
     
         if self.in_motion==0:
@@ -43,15 +52,19 @@ class HMI(object):
             diff = self.time_when_stopped.secs - self.time_when_started_moving.secs
             self.for_waiting=0
             if diff>=5:
+                # self.set_digital_output("y1",False)
                 rospy.loginfo("Parked")
         else:
             self.for_waiting = 1
+            rospy.loginfo("beep")
 if __name__ == "__main__":
+
     rospy.init_node('beep')
-    try:
-        HMI()
-    except rospy.ROSInterruptException:
-        raise e
+    parser = argparse.ArgumentParser(description='delay parser')
+    parser.add_argument('--delay', action="store", dest='delay', default=3)
+    args=parser.parse_args()
+    print (args.delay)
+    beep(args.delay)
     
     rospy.spin()
 
